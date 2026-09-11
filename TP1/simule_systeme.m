8176657c92775a34b1620d9eb1af6003f321ee64
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2026-09-11 11:03:46 +0200

    FEAT: Adds system simulation code with plotting

--- TP1/simule_systeme.m
+++ TP1/simule_systeme.m
@@ -1,16 +1,21 @@
-%% transfer functions
-F1 = [k1+k0 , b0+b1, m1]
-F2 = -[k1, b1];
-F3 = F2;
-F4 = [k1+k2, b1+b2, m2];
-F5 = -[k2, b2];
-F6 = F5;
-F7 = [k2, b2, m3];
+s = tf('s');
 
-G1 = minreal(1/(F1-(F2F3/(F4-F5*F6/F7))));
+%% Transfer functions
+% Cleaning equations 
+F1 = k1+k0 + (b0+b1)*s + m1*s^2;
+F2 = -(k1+b1*s);
+F3 = F2;
+F4 = k1+k2 + (b1+b2)*s + m2*s^2;
+F5 = -(k2+b2*s);
+F6 = F5;
+F7 = k2 + b2*s + m3*s^2;
+
+% Transfer functions
+G1 = minreal(1/(F1-(F2*F3/(F4-F5*F6/F7))));
 G2 = minreal(-F3*G1/(F4-F5*F6/F7));
 G3 = minreal(-F6*G2/F7);
 
+%% Simulations
 t = 0:0.01:8;
 
 alpha = step(G1, t);
@@ -26,3 +31,31 @@ vit_gamma = step(G1 * s, t);
 acc_gamma = step(G1 * s^2, t);
 
 f = F1 * alpha + F2 * beta;
+
+%% Plotting
+subplot(3, 3, 1);
+plot(t,alpha);
+
+subplot(3, 3, 2);
+plot(t,vit_alpha);
+
+subplot(3, 3, 3);
+plot(t,acc_alpha);
+
+subplot(3, 3, 4);
+plot(t,beta);
+
+subplot(3, 3, 5);
+plot(t,vit_beta);
+
+subplot(3, 3, 6);
+plot(t,acc_beta);
+
+subplot(3, 3, 7);
+plot(t,gamma);
+
+subplot(3, 3, 8);
+plot(t,vit_gamma);
+
+subplot(3, 3, 9);
+plot(t,acc_gamma);
