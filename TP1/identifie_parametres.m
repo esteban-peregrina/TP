86b01b1ae3d5eb586fd03c332d3bbd56d3f0b9b8
Leo Sabatie <leo.sabatie@gmail.com>
2026-09-11 10:23:36 +0200

    Feat: Correct identifie_parametres

--- TP1/identifie_parametres.m
+++ TP1/identifie_parametres.m
@@ -20,14 +20,14 @@ cond(A)
 
 p_find = A \ Y;
 
-k0_find = x_find(0);
-k1_find = x_find(1);
-k2_find = x_find(2);
+k0_find = p_find(1)
+k1_find = p_find(2)
+k2_find = p_find(3)
 
-b0_find = x_find(3);
-b1_find = x_find(4);
-b2_find = x_find(5);
+b0_find = p_find(4)
+b1_find = p_find(5)
+b2_find = p_find(6)
 
-m1_find = x_find(6);
-m2_find = x_find(7);
-m3_find = x_find(8);
+m1_find = p_find(7)
+m2_find = p_find(8)
+m3_find = p_find(9)
