bc78cd9ff33df6dbd7a67e3b817eec6ce33d8c34
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2026-09-18 12:09:38 +0200

    FEAT: Complete ident_combine.m

--- TP2/ident_combine.m
+++ TP2/ident_combine.m
@@ -7,49 +7,53 @@ N1=20.25;
 kc2=0.0525;
 N2=4.5;
 
-%% Paramètres identifiés à vitesse constante
+%% Paramï¿½tres identifiï¿½s ï¿½ vitesse constante
 %% Pour l'axe 1 : 
-alpha1=??;
-a1=??;
-b1=??;
-c1=??;
+alpha1=0.8721; % g(m2*l1 + m1*lambda1)   0.872055549717638
+a1=0.1755; % 0.175502156071723
+b1=0.006; % 0.006033143290923
+c1=-0.0302; % -0.030231832523539
 %% Pour l'axe 2 :
-alpha2=??;
-a2=??;
-b2=??;
-c2=??;
+alpha2=0.0829; % g*m2*lambda2  0.082896732238708
+a2=0.0582; % 0.058158202699900
+b2=0.0019; % 0.001935495788991
+c2=-0.0123; % -0.012299158456500
 
-%% identification à partir des données filtrées
+%% identification ï¿½ partir des donnï¿½es filtrï¿½es
 for(i=1:length(t)) 
-    Z(2*i-1:2*i,1:3)=[?? ?? ??
-        ?? ?? ??];
-    u(2*i-1,1)=??;
-    u(2*i,1)=??;
+    Z(2*i-1:2*i,1:3)=[qppfil1(i) (qppfil2(i)*cos(q2(i)-q1(i)))-((qpfil2(i)^2)*sin(q2(i)-q1(i)))  0
+        0 (qppfil1(i)*cos(q2(i)-q1(i)))-((qpfil1(i)^2)*sin(q2(i)-q1(i))) qppfil2(i)];
+    u(2*i-1,1)= N1*kc1*ifil1(i) - alpha1*cos(q1(i)) - a1*sign(qpfil1(i)) - b1*qpfil1(i) - c1;
+    u(2*i,1) = N2*kc2*ifil2(i) - alpha2*cos(q2(i)) - a2*sign(qpfil2(i)) - b2*qpfil2(i) - c2;
 end
-
-p=??;
+disp('Conditionnement :');
+cond(Z)
+disp("Amplification de l'erreur :");
+1./min(svd(Z))
+p=Z\u;
 format long
-disp('Paramètres estimés à partir des données filtrées :');
+disp('Paramï¿½tres estimï¿½s ï¿½ partir des donnï¿½es filtrï¿½es :');
 p'
 
 % reconstruction du modele complet
-p1=p(1);
-p2=p(2);
-p3=p(3);
+p1=p(1); % I1 + m2*l1^2 + Ia1
+p2=p(2); % h
+p3=p(3); % I2 + Ia2
+
 
 for(i=1:length(t)) 
     %% couple d'inertie
-    ciner(2*i-1,1)=??; %% AXE 1 
-    ciner(2*i,1)=??; %%AXE 2
+    ciner(2*i-1,1)= qppfil1(i) * p1 + qppfil2(i) * p2 * cos(q2(i) - q1(i)); %% AXE 1 
+    ciner(2*i,1)= qppfil1(i) * p2 * cos(q2(i) - q1(i)) + qppfil2(i) * p3; %%AXE 2
     %% couple centrifuge
-    ccentri(2*i-1,1)=??; %% AXE 1
-    ccentri(2*i,1)=??; %% AXE 2
-    %% couple de gravité
-    cgravi(2*i-1,1) = ??; %% AXE 1
-    cgravi(2*i,1) = ??; %% AXE 2
+    ccentri(2*i-1,1)= - qpfil2(i)^2 * p2 * sin(q2(i) - q1(i)); %% AXE 1
+    ccentri(2*i,1)= qpfil1(i)^2 * p2 * sin(q2(i) - q1(i)); %% AXE 2
+    %% couple de gravitï¿½
+    cgravi(2*i-1,1) = alpha1 * cos(q1(i)); %% AXE 1
+    cgravi(2*i,1) = alpha2 * cos(q2(i)); %% AXE 2
     %% couple de frottements
-    cfrott(2*i-1,1)=??; %% AXE 1
-    cfrott(2*i,1)=??; %% AXE 2
+    cfrott(2*i-1,1)= a1 * sign(qpfil1(i)) + b1 * qpfil1(i) + c1; %% AXE 1
+    cfrott(2*i,1)= a2 * sign(qpfil2(i)) + b2 * qpfil2(i) + c2; %% AXE 2
     %% couple total
     ctotal(2*i-1:2*i,1)=ciner(2*i-1:2*i,1)+ccentri(2*i-1:2*i,1)+cgravi(2*i-1:2*i,1)+cfrott(2*i-1:2*i,1);
 end
@@ -72,8 +76,8 @@ h=plot(t,cfrott(1:2:length(ctotal)),'g');
 set(h,'LineWidth',1.);
 h=plot(t,ctotal(1:2:length(ctotal)),'c--');
 set(h,'LineWidth',1.5);
-legend('\Gamma_1 mesuré','\Gamma_1 filtré','inertie','gravité','centrifuge','frottements','modèle total');
-title('Résultats axe 1 ; identification à partir de données filtrées');
+legend('\Gamma_1 mesurï¿½','\Gamma_1 filtrï¿½','inertie','gravitï¿½','centrifuge','frottements','modï¿½le total');
+title('Rï¿½sultats axe 1 ; identification ï¿½ partir de donnï¿½es filtrï¿½es');
 figure(2)
 clf
 hold on
@@ -91,5 +95,5 @@ h=plot(t,cfrott(2:2:length(ctotal)),'g');
 set(h,'LineWidth',1);
 h=plot(t,ctotal(2:2:length(ctotal)),'c--');
 set(h,'LineWidth',1.5);
-legend('\Gamma_2 mesuré','\Gamma_2 filtré','inertie','gravité','centrifuge','frottements','modèle total');
-title('Résultats axe 2 ; identification à partir de données filtrées');
+legend('\Gamma_2 mesurï¿½','\Gamma_2 filtrï¿½','inertie','gravitï¿½','centrifuge','frottements','modï¿½le total');
+title('Rï¿½sultats axe 2 ; identification ï¿½ partir de donnï¿½es filtrï¿½es');
